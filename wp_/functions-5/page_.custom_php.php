532c47372e91663928b4face86178dd2660c1beb
Pondus83 <55687200+Pondus83@users.noreply.github.com>
2020-06-11 14:59:38 +0200

    wp commit

---
+++ wp_/functions-5/page_.custom_php.php
@@ -0,0 +1,25 @@
+<?php
+/**
+Template Name: Custom PHP
+ */
+
+get_header(); ?>
+  <div class="main-container">
+<div class="content-area">
+    <div class="middle-align content_sidebar">
+        <div class="site-main" id="sitemain">
+			<?php while ( have_posts() ) : the_post(); ?>
+                <?php get_template_part( 'content', 'page' ); ?>
+                <?php
+                //If comments are open or we have at least one comment, load up the comment template
+                	if ( comments_open() || '0' != get_comments_number() )
+                    	comments_template();
+                ?>
+            <?php endwhile; // end of the loop. ?>
+        </div>
+        <?php// get_sidebar(); ?>
+        <div class="clear"></div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
